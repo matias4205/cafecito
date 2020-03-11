b0fb9f41a6580564a09272286967abff7abfffd0
matias4205 <matiasperezpc@hotmail.com>
2020-03-11 03:12:14 -0300

    Minor changes to personalize

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <title>Damián Catanzaro | Cafecito</title>
+                <title>Matias Perez | Cafecito</title>
             </Head>
             <Component {...pageProps} />
         </>
